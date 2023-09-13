37f3f6666d1500957fb4f125fd218551b5626e28
felo <https://github.com/felobesabd>
2023-09-13 17:14:41 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {TranslateService} from "@ngx-translate/core";
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'task-app';
+  lang: any;
+  rtl:boolean = false;
+
+  constructor(private translate: TranslateService) {
+    if ('language' in localStorage) {
+      this.lang = localStorage.getItem('language')
+
+      if (this.lang == 'ar'){
+        this.rtl = true;
+      }
+
+      translate.use(this.lang);
+    } else {
+      translate.use(this.translate.defaultLang);
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,51 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {HeaderComponent} from "../components/header/header.component";
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {MatIconModule} from '@angular/material/icon';
+import {MaterialModule} from "../material/material.module";
+import {InfoComponent} from "../components/info/info.component";
+import {SelectResComponent} from "../components/select-res/select-res.component";
+import {DeadlineComponent} from "../components/deadline/deadline.component";
+import {TimeworkComponent} from "../components/timework/timework.component";
+import { ReactiveFormsModule } from '@angular/forms';
+import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    InfoComponent,
+    SelectResComponent,
+    DeadlineComponent,
+    TimeworkComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatSlideToggleModule,
+    MatIconModule,
+    MaterialModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      defaultLanguage: 'en',
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
+  return new TranslateHttpLoader(http);
+}
